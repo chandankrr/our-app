34e7e366eeed3189404e0ed837c235ee886391cc
Chandan Kumar <chandankrr.91@gmail.com>
2022-10-29 17:00:15 +0530

    added responsive style to login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import './style.scss';
 
 function App() {
   const router = createBrowserRouter([
