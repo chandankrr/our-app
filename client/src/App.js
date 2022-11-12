80abeec8c70886b8ad2a2cf73309dd6ca4606919
Chandan Kumar <chandankrr.91@gmail.com>
2022-11-12 20:13:18 +0530

    edited path for home and profile pages

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ import {
 import LeftBar from './components/leftBar/LeftBar';
 import Navbar from './components/navbar/Navbar';
 import RightBar from './components/rightBar/RightBar';
-import Home from './home/Home';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
 import Register from './pages/register/Register';
-import Profile from './profile/Profile';
 import './style.scss';
 
 function App() {
